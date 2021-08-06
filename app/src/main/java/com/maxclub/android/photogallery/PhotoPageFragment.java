951bb401596d0c76b9098ef4bedb99180a512f01
MaxKliuba <klyubamaks@gmail.com>
2021-08-06 14:33:27 +0300

    onBackPressed overridden, url scheme validation added

--- app/src/main/java/com/maxclub/android/photogallery/PhotoPageFragment.java
+++ app/src/main/java/com/maxclub/android/photogallery/PhotoPageFragment.java
@@ -1,15 +1,24 @@
 package com.maxclub.android.photogallery;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.URLUtil;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +28,8 @@ import com.google.android.material.progressindicator.LinearProgressIndicator;
 import org.jetbrains.annotations.NotNull;
 
 public class PhotoPageFragment extends VisibleFragment {
+
+    private static final String TAG = "PhotoPageFragment";
 
     private static final String ARG_URI = "photo_page_url";
 
@@ -38,6 +49,8 @@ public class PhotoPageFragment extends VisibleFragment {
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
         mUri = (Uri) getArguments().getParcelable(ARG_URI);
     }
 
@@ -69,12 +82,64 @@ public class PhotoPageFragment extends VisibleFragment {
             @Override
             public void onReceivedTitle(WebView view, String title) {
                 AppCompatActivity activity = (AppCompatActivity) getActivity();
-                activity.getSupportActionBar().setSubtitle(title);
+                if (activity != null) {
+                    activity.getSupportActionBar().setSubtitle(title);
+                }
+            }
+
+
+        });
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                if (URLUtil.isNetworkUrl(url)) {
+                    return false;
+                }
+
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                } catch (ActivityNotFoundException anfe) {
+                    Log.e(TAG, anfe.getMessage(), anfe);
+                    mWebView.stopLoading();
+                    getActivity().onBackPressed();
+                    Toast.makeText(getActivity(), R.string.app_not_found, Toast.LENGTH_LONG).show();
+                }
+
+                return true;
             }
         });
-        mWebView.setWebViewClient(new WebViewClient());
         mWebView.loadUrl(mUri.toString());
 
         return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull @NotNull Context context) {
+        super.onAttach(context);
+
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (mWebView.canGoBack()) {
+                    mWebView.goBack();
+                } else {
+                    setEnabled(false);
+                    requireActivity().onBackPressed();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull @NotNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getActivity().finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
